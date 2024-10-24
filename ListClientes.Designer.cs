1520a7758e490de4b706c6f73e3599e7c7c1fb18
Rômulo Rodrigues <rodriguesp.romulo@gmail.com>
2024-10-24 18:19:57 -0300

    lista de clientes criada

--- ListClientes.Designer.cs
+++ ListClientes.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace TelasDesktopPIM
 {
-    partial class Gestao
+    partial class ListClientes
     {
         /// <summary>
         /// Required designer variable.
@@ -31,15 +31,19 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.buttonSair = new System.Windows.Forms.Button();
             this.buttonPerfil = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.buttonRelatorio = new System.Windows.Forms.Button();
             this.buttonVendas = new System.Windows.Forms.Button();
             this.buttonProducao = new System.Windows.Forms.Button();
             this.buttonGestão = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.labelGestao = new System.Windows.Forms.Label();
-            this.buttonGerFuncionario = new System.Windows.Forms.Button();
-            this.buttonGerClient = new System.Windows.Forms.Button();
-            this.buttonGerProdut = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -54,10 +58,10 @@
             this.panel1.Controls.Add(this.buttonVendas);
             this.panel1.Controls.Add(this.buttonProducao);
             this.panel1.Controls.Add(this.buttonGestão);
-            this.panel1.Location = new System.Drawing.Point(1, 1);
+            this.panel1.Location = new System.Drawing.Point(1, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(798, 63);
-            this.panel1.TabIndex = 3;
+            this.panel1.Size = new System.Drawing.Size(798, 62);
+            this.panel1.TabIndex = 22;
             // 
             // buttonSair
             // 
@@ -82,7 +86,16 @@
             this.buttonPerfil.TabIndex = 4;
             this.buttonPerfil.Text = "Perfil";
             this.buttonPerfil.UseVisualStyleBackColor = false;
-            this.buttonPerfil.Click += new System.EventHandler(this.buttonPerfil_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::TelasDesktopPIM.Properties.Resources.LogoHorizontal_250;
+            this.pictureBox1.Location = new System.Drawing.Point(558, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(195, 64);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
             // 
             // buttonRelatorio
             // 
@@ -95,7 +108,6 @@
             this.buttonRelatorio.TabIndex = 3;
             this.buttonRelatorio.Text = "Relatorio";
             this.buttonRelatorio.UseVisualStyleBackColor = false;
-            this.buttonRelatorio.Click += new System.EventHandler(this.buttonRelatorio_Click);
             // 
             // buttonVendas
             // 
@@ -108,7 +120,6 @@
             this.buttonVendas.TabIndex = 3;
             this.buttonVendas.Text = "Vendas";
             this.buttonVendas.UseVisualStyleBackColor = false;
-            this.buttonVendas.Click += new System.EventHandler(this.buttonVendas_Click);
             // 
             // buttonProducao
             // 
@@ -121,7 +132,6 @@
             this.buttonProducao.TabIndex = 3;
             this.buttonProducao.Text = "Produção";
             this.buttonProducao.UseVisualStyleBackColor = false;
-            this.buttonProducao.Click += new System.EventHandler(this.buttonProducao_Click);
             // 
             // buttonGestão
             // 
@@ -134,70 +144,106 @@
             this.buttonGestão.TabIndex = 1;
             this.buttonGestão.Text = "Gestão";
             this.buttonGestão.UseVisualStyleBackColor = false;
-            this.buttonGestão.Click += new System.EventHandler(this.buttonGestão_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(464, 187);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(169, 21);
+            this.textBox3.TabIndex = 21;
+            this.textBox3.Text = "DD/MM/AAAA";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(250, 187);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(169, 21);
+            this.textBox2.TabIndex = 20;
+            this.textBox2.Text = "Funcionario 1 de Oliveira";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(81, 187);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(96, 21);
+            this.textBox1.TabIndex = 19;
+            this.textBox1.Text = "123.456.789-22";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkGreen;
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(495, 89);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(168, 44);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Cadastrar cliente";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Green;
+            this.label3.Location = new System.Drawing.Point(479, 159);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 16);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Data de Registro";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Green;
+            this.label2.Location = new System.Drawing.Point(262, 159);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 16);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Nome do Cliente";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Green;
+            this.label1.Location = new System.Drawing.Point(98, 159);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 16);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "CPF/CNPJ";
             // 
             // labelGestao
             // 
             this.labelGestao.AutoSize = true;
             this.labelGestao.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelGestao.ForeColor = System.Drawing.Color.DarkGreen;
-            this.labelGestao.Location = new System.Drawing.Point(313, 100);
+            this.labelGestao.Location = new System.Drawing.Point(82, 89);
             this.labelGestao.Name = "labelGestao";
-            this.labelGestao.Size = new System.Drawing.Size(96, 28);
-            this.labelGestao.TabIndex = 4;
-            this.labelGestao.Text = "Gestão";
+            this.labelGestao.Size = new System.Drawing.Size(214, 28);
+            this.labelGestao.TabIndex = 14;
+            this.labelGestao.Text = "LISTA DE CLIENTES";
             // 
-            // buttonGerFuncionario
-            // 
-            this.buttonGerFuncionario.Location = new System.Drawing.Point(289, 166);
-            this.buttonGerFuncionario.Name = "buttonGerFuncionario";
-            this.buttonGerFuncionario.Size = new System.Drawing.Size(159, 23);
-            this.buttonGerFuncionario.TabIndex = 5;
-            this.buttonGerFuncionario.Text = "Gerenciar Funcionarios";
-            this.buttonGerFuncionario.UseVisualStyleBackColor = true;
-            this.buttonGerFuncionario.Click += new System.EventHandler(this.buttonGerFuncionario_Click);
-            // 
-            // buttonGerClient
-            // 
-            this.buttonGerClient.Location = new System.Drawing.Point(289, 220);
-            this.buttonGerClient.Name = "buttonGerClient";
-            this.buttonGerClient.Size = new System.Drawing.Size(159, 23);
-            this.buttonGerClient.TabIndex = 6;
-            this.buttonGerClient.Text = "Gerenciar Clientes";
-            this.buttonGerClient.UseVisualStyleBackColor = true;
-            // 
-            // buttonGerProdut
-            // 
-            this.buttonGerProdut.Location = new System.Drawing.Point(289, 273);
-            this.buttonGerProdut.Name = "buttonGerProdut";
-            this.buttonGerProdut.Size = new System.Drawing.Size(159, 23);
-            this.buttonGerProdut.TabIndex = 7;
-            this.buttonGerProdut.Text = "Gerenciar Produtos";
-            this.buttonGerProdut.UseVisualStyleBackColor = true;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::TelasDesktopPIM.Properties.Resources.LogoHorizontal_250;
-            this.pictureBox1.Location = new System.Drawing.Point(558, -1);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(195, 64);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
-            this.pictureBox1.TabIndex = 3;
-            this.pictureBox1.TabStop = false;
-            // 
-            // Gestao
+            // ListClientes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.buttonGerProdut);
-            this.Controls.Add(this.buttonGerClient);
-            this.Controls.Add(this.buttonGerFuncionario);
-            this.Controls.Add(this.labelGestao);
             this.Controls.Add(this.panel1);
-            this.Name = "Gestao";
-            this.Text = "Gestao";
-            this.Load += new System.EventHandler(this.Gestao_Load);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelGestao);
+            this.Name = "ListClientes";
+            this.Text = "ListClientes";
             this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
@@ -215,9 +261,13 @@
         private System.Windows.Forms.Button buttonVendas;
         private System.Windows.Forms.Button buttonProducao;
         private System.Windows.Forms.Button buttonGestão;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label labelGestao;
-        private System.Windows.Forms.Button buttonGerFuncionario;
-        private System.Windows.Forms.Button buttonGerClient;
-        private System.Windows.Forms.Button buttonGerProdut;
     }
 }
