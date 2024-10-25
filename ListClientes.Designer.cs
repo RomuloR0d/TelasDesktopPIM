26d404cf260f884a2400a50ef647313d1bd8501f
Romulo Pereira <romulo.pereira10@aluno.unip.br>
2024-10-25 19:33:57 -0300

    remoça~ão de cadastro cliente

--- ListClientes.Designer.cs
+++ ListClientes.Designer.cs
@@ -37,7 +37,6 @@
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
@@ -55,8 +54,9 @@
             this.panel1.Controls.Add(this.buttonVendas);
             this.panel1.Controls.Add(this.buttonGestão);
             this.panel1.Location = new System.Drawing.Point(1, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(798, 62);
+            this.panel1.Size = new System.Drawing.Size(1064, 76);
             this.panel1.TabIndex = 22;
             // 
             // buttonSair
@@ -64,9 +64,10 @@
             this.buttonSair.BackColor = System.Drawing.Color.ForestGreen;
             this.buttonSair.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonSair.ForeColor = System.Drawing.SystemColors.Control;
-            this.buttonSair.Location = new System.Drawing.Point(759, 22);
+            this.buttonSair.Location = new System.Drawing.Point(1012, 27);
+            this.buttonSair.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonSair.Name = "buttonSair";
-            this.buttonSair.Size = new System.Drawing.Size(39, 26);
+            this.buttonSair.Size = new System.Drawing.Size(52, 32);
             this.buttonSair.TabIndex = 5;
             this.buttonSair.Text = "Sair";
             this.buttonSair.UseVisualStyleBackColor = false;
@@ -76,9 +77,10 @@
             this.buttonPerfil.BackColor = System.Drawing.Color.ForestGreen;
             this.buttonPerfil.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonPerfil.ForeColor = System.Drawing.SystemColors.Control;
-            this.buttonPerfil.Location = new System.Drawing.Point(371, 22);
+            this.buttonPerfil.Location = new System.Drawing.Point(495, 27);
+            this.buttonPerfil.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonPerfil.Name = "buttonPerfil";
-            this.buttonPerfil.Size = new System.Drawing.Size(93, 26);
+            this.buttonPerfil.Size = new System.Drawing.Size(124, 32);
             this.buttonPerfil.TabIndex = 4;
             this.buttonPerfil.Text = "Perfil";
             this.buttonPerfil.UseVisualStyleBackColor = false;
@@ -87,9 +89,10 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::TelasDesktopPIM.Properties.Resources.LogoHorizontal_250;
-            this.pictureBox1.Location = new System.Drawing.Point(517, 0);
+            this.pictureBox1.Location = new System.Drawing.Point(689, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(195, 64);
+            this.pictureBox1.Size = new System.Drawing.Size(260, 79);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
@@ -100,9 +103,10 @@
             this.buttonVendas.BackColor = System.Drawing.Color.ForestGreen;
             this.buttonVendas.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonVendas.ForeColor = System.Drawing.SystemColors.Control;
-            this.buttonVendas.Location = new System.Drawing.Point(229, 22);
+            this.buttonVendas.Location = new System.Drawing.Point(305, 27);
+            this.buttonVendas.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonVendas.Name = "buttonVendas";
-            this.buttonVendas.Size = new System.Drawing.Size(93, 26);
+            this.buttonVendas.Size = new System.Drawing.Size(124, 32);
             this.buttonVendas.TabIndex = 3;
             this.buttonVendas.Text = "Vendas";
             this.buttonVendas.UseVisualStyleBackColor = false;
@@ -113,9 +117,10 @@
             this.buttonGestão.BackColor = System.Drawing.Color.ForestGreen;
             this.buttonGestão.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonGestão.ForeColor = System.Drawing.SystemColors.Control;
-            this.buttonGestão.Location = new System.Drawing.Point(86, 22);
+            this.buttonGestão.Location = new System.Drawing.Point(115, 27);
+            this.buttonGestão.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonGestão.Name = "buttonGestão";
-            this.buttonGestão.Size = new System.Drawing.Size(93, 26);
+            this.buttonGestão.Size = new System.Drawing.Size(124, 32);
             this.buttonGestão.TabIndex = 1;
             this.buttonGestão.Text = "Gestão";
             this.buttonGestão.UseVisualStyleBackColor = false;
@@ -124,50 +129,42 @@
             // textBox3
             // 
             this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox3.Location = new System.Drawing.Point(464, 187);
+            this.textBox3.Location = new System.Drawing.Point(619, 230);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(169, 21);
+            this.textBox3.Size = new System.Drawing.Size(224, 24);
             this.textBox3.TabIndex = 21;
             this.textBox3.Text = "DD/MM/AAAA";
             // 
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(250, 187);
+            this.textBox2.Location = new System.Drawing.Point(333, 230);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(169, 21);
+            this.textBox2.Size = new System.Drawing.Size(224, 24);
             this.textBox2.TabIndex = 20;
             this.textBox2.Text = "Funcionario 1 de Oliveira";
             // 
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(81, 187);
+            this.textBox1.Location = new System.Drawing.Point(108, 230);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(96, 21);
+            this.textBox1.Size = new System.Drawing.Size(127, 24);
             this.textBox1.TabIndex = 19;
             this.textBox1.Text = "123.456.789-22";
-            // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.Color.DarkGreen;
-            this.button1.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.SystemColors.Control;
-            this.button1.Location = new System.Drawing.Point(495, 89);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(168, 44);
-            this.button1.TabIndex = 18;
-            this.button1.Text = "Cadastrar cliente";
-            this.button1.UseVisualStyleBackColor = false;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.Green;
-            this.label3.Location = new System.Drawing.Point(479, 159);
+            this.label3.Location = new System.Drawing.Point(639, 196);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(115, 16);
+            this.label3.Size = new System.Drawing.Size(143, 19);
             this.label3.TabIndex = 17;
             this.label3.Text = "Data de Registro";
             // 
@@ -176,9 +173,10 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.Green;
-            this.label2.Location = new System.Drawing.Point(262, 159);
+            this.label2.Location = new System.Drawing.Point(349, 196);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(116, 16);
+            this.label2.Size = new System.Drawing.Size(149, 19);
             this.label2.TabIndex = 16;
             this.label2.Text = "Nome do Cliente";
             // 
@@ -187,9 +185,10 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Green;
-            this.label1.Location = new System.Drawing.Point(98, 159);
+            this.label1.Location = new System.Drawing.Point(131, 196);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(69, 16);
+            this.label1.Size = new System.Drawing.Size(92, 19);
             this.label1.TabIndex = 15;
             this.label1.Text = "CPF/CNPJ";
             // 
@@ -198,26 +197,27 @@
             this.labelGestao.AutoSize = true;
             this.labelGestao.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelGestao.ForeColor = System.Drawing.Color.DarkGreen;
-            this.labelGestao.Location = new System.Drawing.Point(82, 89);
+            this.labelGestao.Location = new System.Drawing.Point(109, 110);
+            this.labelGestao.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelGestao.Name = "labelGestao";
-            this.labelGestao.Size = new System.Drawing.Size(214, 28);
+            this.labelGestao.Size = new System.Drawing.Size(269, 37);
             this.labelGestao.TabIndex = 14;
             this.labelGestao.Text = "LISTA DE CLIENTES";
             // 
             // ListClientes
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.labelGestao);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "ListClientes";
             this.Text = "ListClientes";
             this.panel1.ResumeLayout(false);
@@ -238,7 +238,6 @@
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
